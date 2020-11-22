8194589e064d3b0efd795395b0a41c9079d7fb1d
Iida Hämäläinen <iida.t.hamalainen@gmail.com>
2020-11-22 16:54:32 +0200

    added package io and command for adding book

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
+
+import java.util.Scanner;
+import io.ConsoleIO;
+import io.IO;
+
 public class Main {
+    
+     private IO io;
+     
     public static void main(String[] args) {
-        System.out.println("Hello gradle!");
+          
+        System.out.println("Welcome to BookMarkApp!");
+       
+        IO io = new ConsoleIO();
+        new App(io).run();
+       
+        
+         
+   
     }
+    
 }
